28638b37d7caa6f29130f02d3f30d1d7c9832430
Richasrd <richardhenry602@gmail.com>
2016-12-04 15:59:59 +0000

    Added NumberTextField, as an extention of TextField

---
+++ src/circularmotion/NumberTextField.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package circularmotion;
+
+import javafx.scene.control.TextField;
+
+/**
+ * @author 		Richard Henry (richardhenry602@gmail.com)
+ * @since 		4 Dec 2016
+ *
+ */
+public class NumberTextField extends TextField {
+	// NumberTextField as suggested by @Burkhard at http://stackoverflow.com/a/18959399
+	// With modifications as necissary.
+    @Override
+    public void replaceText(int start, int end, String text){
+        if (validate(text)){
+            super.replaceText(start, end, text);
+        }
+    }
+
+    @Override
+    public void replaceSelection(String text) {
+        if (validate(text)) {
+            super.replaceSelection(text);
+        }
+    }
+    
+    private boolean validate(String text) {
+        return text.matches("[0-9]*");
+    }
+    public void setText(double contents){
+    	setText(Double.toString(contents));
+    }
+    public NumberTextField(double contents){
+    	super(Double.toString(contents));
+    }
+}
