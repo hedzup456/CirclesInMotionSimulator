ff4a0b3b931edad7b33922f7bf158163aa0e177f
Richard Henry <richardhenry602@gmail.com>
2016-12-06 14:28:23 +0000

    AH

--- src/circularmotion/NumberTextField.java
+++ src/circularmotion/NumberTextField.java
@@ -4,6 +4,7 @@
 package circularmotion;
 
 import javafx.scene.control.TextField;
+import javafx.scene.media.AudioClip;
 
 /**
  * @author 		Richard Henry (richardhenry602@gmail.com)
@@ -13,6 +14,7 @@ import javafx.scene.control.TextField;
 public class NumberTextField extends TextField {
 	// NumberTextField as suggested by @Burkhard at http://stackoverflow.com/a/18959399
 	// With modifications as necessary.
+    private boolean dp = false;
     @Override
     public void replaceText(int start, int end, String text){
         if (validate(text)){
@@ -26,9 +28,28 @@ public class NumberTextField extends TextField {
             super.replaceSelection(text);
         }
     }
-    
+
+    /**
+     * Simple method to play a warning bell to the user.
+     *
+     * Customise the URL String in the AudioClip constructor to use different sounds.
+     */
+    private void bell(){
+        AudioClip bell = new AudioClip("http://localhost:8000/beep-02.wav");    // Use python -m http.server in
+        // res folder.
+        bell.play();
+    }
+
     private boolean validate(String text) {
-        return text.matches("\\d+\\.?\\d*");
+        if (text.matches("\\.") && !dp){
+            dp = true;
+            return true;
+        } else if (text.matches("\\d")){
+            return true;
+        } else {
+            if (text.contains("\\b")) bell();
+            return false;
+        }
     }
     public void setText(double contents){
     	setText(Double.toString(contents));
