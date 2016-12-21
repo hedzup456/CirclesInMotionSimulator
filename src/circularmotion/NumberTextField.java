bc52ddaa4408c077eb2dcf11c1ee788557c1c3b2
Richasrd <richardhenry602@gmail.com>
2016-12-21 23:17:33 +0000

    Added workaround for the regex issues.

--- src/circularmotion/NumberTextField.java
+++ src/circularmotion/NumberTextField.java
@@ -6,15 +6,23 @@ package circularmotion;
 import javafx.scene.control.TextField;
 import javafx.scene.media.AudioClip;
 
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.Random;
+
 /**
  * @author 		Richard Henry (richardhenry602@gmail.com)
  * @since 		4 Dec 2016
  *
  */
 public class NumberTextField extends TextField {
-	// NumberTextField as suggested by @Burkhard at http://stackoverflow.com/a/18959399
+	public NumberTextField(){
+	    this(0.0);
+    }
+    // NumberTextField as suggested by @Burkhard at http://stackoverflow.com/a/18959399
 	// With modifications as necessary.
-    private boolean dp = false;
+    private boolean dp = true;
     @Override
     public void replaceText(int start, int end, String text){
         if (validate(text)){
@@ -28,26 +36,40 @@ public class NumberTextField extends TextField {
             super.replaceSelection(text);
         }
     }
+    public double getValue(){
+        String text = this.getText();
+        return Double.parseDouble(text);
+    }
 
     /**
      * Simple method to play a warning bell to the user.
      *
      * Customise the URL String in the AudioClip constructor to use different sounds.
      */
-    private void bell(){
-        AudioClip bell = new AudioClip("http://localhost:8000/beep-02.wav");    // Use python -m http.server in
-        // res folder.
+    public void bell(){
+        Random rng = new Random(); // Added for entertainment purposes when debugging. Will be removed, along with it's
+        // use on line +3
+        String sndstrm= getClass().getClassLoader().getResource("./res/dss.wav").toString();
+        String beepsfx = getClass().getClassLoader().getResource("./res/beep-02.wav").toString();
+        AudioClip bell = new AudioClip(((rng.nextBoolean()? sndstrm : beepsfx)));   // Randomly decides whether to use
+        // the beep sound effect or Darude - Sandstorm.
+        System.out.println("The system has belled.");
         bell.play();
     }
 
     private boolean validate(String text) {
-        if (text.matches("\\.") && !dp){
+        if (text.length() == 0){
+            String contents = getText();
+            dp = !(contents.charAt(contents.length() - 1) == '.');
+            return true;
+        }
+        if (text.matches("\\.") && !dp){    // If the character is a decimal point and etc
             dp = true;
             return true;
-        } else if (text.matches("\\d")){
+        } else if (text.matches("\\d")){    // If the character is a digit
             return true;
         } else {
-            if (text.contains("\\b")) bell();
+            bell();
             return false;
         }
     }
