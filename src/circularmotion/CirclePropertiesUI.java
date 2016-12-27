e72ffac00057d2cadbde3b507089f2b4f75fe955
Richasrd <richardhenry602@gmail.com>
2016-12-27 15:35:22 +0000

    Neatened up the code a bit to improve readability.

--- src/circularmotion/CirclePropertiesUI.java
+++ src/circularmotion/CirclePropertiesUI.java
@@ -12,6 +12,12 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.awt.image.BufferedImage;
+
+// This is literally done for no reason other than more class files = better.
+// Okay also because it's neater to have it as a separate object, and it's easier to modify and stuff.
+// But shush.
+
 /**
  * Created by richa on 27/12/2016.
  */
@@ -19,10 +25,27 @@ public class CirclePropertiesUI {
     Stage circlePopUp = new Stage();
     SingleParticle particle;
 
-    public CirclePropertiesUI(SingleParticle sp){
-        particle = sp;
+    /**
+     * Constructor for the CirclePropertiesUI object.
+     * @param existingParticle The particle object on which to base the existing values in the popup.
+     */
+    public CirclePropertiesUI(SingleParticle existingParticle){
+        particle = existingParticle;
         setUpNewCirclePropertiesPopup();
     }
+
+    /**
+     * Constructor for the CirclePropertiesUI object.
+     * <p>
+     *     This creates a default CirclePropertiesUI without any initialised values. Just in case.
+     * </p>
+     */
+    public  CirclePropertiesUI(){
+        this(new SingleParticle());
+    }
+    /**
+     *
+     */
     public void show(){
         circlePopUp.show();
     }
@@ -57,69 +80,77 @@ public class CirclePropertiesUI {
         VBox components = new VBox();
         components.setAlignment(Pos.TOP_CENTER);
 
+
         Label lblRadius = new Label("Radius");
         NumberTextField radius = new NumberTextField(particle.getRadius());
         radius.setTooltip(new Tooltip("Set the desired radius of your circle."));
 
+
         NumberTextField periodField = new NumberTextField(particle.getPeriod());
         Label lblPeriodOrFreq = new Label("Period or Frequecy");
         HBox periodOrFrequencyBox = new HBox();
+
         periodOrFrequencyBox.setAlignment(Pos.CENTER);
         ToggleGroup perOrFreq = new ToggleGroup();
+
+        EventHandler<ActionEvent> handlePeriodOrFrequency = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle (ActionEvent event) {
+                if(((Button)event.getSource()).getId().equals("Frequency")) periodField.setText(particle.getFrequency());
+                else periodField.setText(particle.getPeriod());
+            }
+        };
+
         ToggleButton period = new ToggleButton("Period");
+        period.setId("Period");
         period.setToggleGroup(perOrFreq);
         period.setSelected(true);
-        period.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                periodField.setText(particle.getPeriod());
-            }
-        });
+        period.setOnAction(handlePeriodOrFrequency);
+
         ToggleButton frequency = new ToggleButton("Frequency");
+        frequency.setId("Frequency");
         frequency.setToggleGroup(perOrFreq);
-        frequency.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                periodField.setText(particle.getFrequency());
-            }
-        });
+        frequency.setOnAction(handlePeriodOrFrequency);
+
         periodOrFrequencyBox.getChildren().addAll(period, frequency);
 
         Label lblAcc = new Label("Acceleration");
         NumberTextField acc = new NumberTextField(particle.getAcceleration());
         acc.setTooltip(new Tooltip("Set the desired acceleration of your particle."));
 
+
+        EventHandler<ActionEvent> handleClosing = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle (ActionEvent event) {
+                Button src = (Button) event.getSource();
+                if ( src.getId().equals("Okay") ) saveCircleSettings( radius, period.isSelected(), periodField, acc );
+                circlePopUp.hide();
+            }
+        };
+
         HBox cancelOkay = new HBox();
         cancelOkay.setAlignment(Pos.CENTER);
+
         Button cancel = new Button("Cancel");
+        cancel.setId("Cancel");
         cancel.setTooltip(new Tooltip("This will discard your changes and close the edit window."));
-        cancel.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                Platform.exit(); // Cancel and exit the program.
-                // TODO REMOVE EXIT WHEN DONE
-                circlePopUp.hide();
-            }
-        });
+        cancel.setOnAction(handleClosing);
+
         Button okay = new Button("Okay");
+        okay.setId("Okay");
         okay.setTooltip(new Tooltip("This will save your changes and reflect them in the main display."));
-        okay.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                saveCircleSettings(radius, period.isSelected(), periodField, acc);
-                System.out.println("ok");
-                circlePopUp.hide();
-            }
-        });
+        okay.setOnAction(handleClosing);
+
         cancelOkay.getChildren().addAll(cancel, okay);
 
-        components.getChildren().addAll(lblRadius, radius, lblPeriodOrFreq, periodOrFrequencyBox, periodField, lblAcc,
-                acc, cancelOkay);
+
+        components.getChildren().addAll(
+                lblRadius, radius, lblPeriodOrFreq, periodOrFrequencyBox, periodField, lblAcc, acc, cancelOkay);
 
         Scene stageScene = new Scene(components, 300, 300);
         circlePopUp.setScene(stageScene);
         circlePopUp.setAlwaysOnTop(true);
-        circlePopUp.show();
         // acceleration
     }
+
 }
